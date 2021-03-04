a85e8ef78e7f3d60b375378928a67757ce50224e
clayton <me@clayton.cc>
2021-03-03 17:42:54 -0800

    Fix ProtectedRoute

--- client/src/ProtectedRoute.tsx
+++ client/src/ProtectedRoute.tsx
@@ -4,17 +4,19 @@ import { loginUrl, useOsuAuth } from './osuAuth';
 import { canReadAs } from './permissions';
 
 export function ProtectedRoute(props: RouteProps & { role?: IRole | 'any' }) {
-  const user = useOsuAuth().user;
+  const authUser = useOsuAuth().user;
+  const children = props.children;
+  delete props.children;
 
   return (
     <Route
       {...props}
       render={() => {
-        if (user == null)
+        if (authUser == null)
           return <p>I don't know who you are! Try <a href={loginUrl}>logging in</a> first.</p>;
 
-        if (props.role == null || canReadAs(user, props.role))
-          return props.children;
+        if (props.role == null || canReadAs(authUser, props.role))
+          return children;
 
         return <p>You aren't cool enough to see this page.</p>
       }}
