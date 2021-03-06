4dcea347e0ec845301ea1327df44b100c4105124
clayton <me@clayton.cc>
2021-03-06 15:43:27 -0800

    Update header and styles

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,23 +1,29 @@
 import { NavLink, Link } from 'react-router-dom';
 import { loginUrl, useOsuAuth } from './osuAuth';
+import { canReadAs } from './permissions';
+import { UserInline } from './UserInline';
 
 export function Header() {
-  const user = useOsuAuth().user;
+  const { logOut, user } = useOsuAuth();
 
   return (
-    <header className='big-center container'>
+    <header className='big-center'>
       <div className='info'>
         <Link className='logo' to='/'>Project Loved</Link>
+        <span>Language: English</span>
         {user == null
           ? <a href={loginUrl}>Login with osu!</a>
-          : <span>Logged in as {user.name} [#{user.id}]</span>
+          : <span>
+              <UserInline user={user} /> — {' '}
+              <button className='fake-a' onClick={logOut}>Log out</button>
+            </span>
         }
       </div>
       <nav>
-        <NavLink to='/'>Submissions</NavLink>
+        <NavLink exact to='/'>Submissions</NavLink>
         <NavLink to='/submit'>Submit a map</NavLink>
         <NavLink to='/captains'>Captains</NavLink>
-        {user != null &&
+        {user != null && canReadAs(user, 'any') &&
           <>
             <span>Admin:</span>
             <NavLink to='/admin/picks'>Picks</NavLink>
--- client/src/UserInline.tsx
+++ client/src/UserInline.tsx
@@ -6,12 +6,17 @@ type UserInlineProps = {
 
 export function UserInline(props: UserInlineProps) {
   return (
-    <span>
+    <a href={`https://osu.ppy.sh/users/${props.user.id}`}>
       <img
-        src={`https://osu.ppy.sh/images/flags/${props.user.country}.png`}
         alt={`${props.user.country} flag`}
+        src={`https://osu.ppy.sh/images/flags/${props.user.country}.png`}
+        style={{
+          height: '1em',
+          paddingBottom: '0.05em',
+          verticalAlign: 'bottom',
+        }}
       />
-      {props.user.name} [#{props.user.id}]
-    </span>
+      {' '} {props.user.name} [#{props.user.id}]
+    </a>
   );
 }
