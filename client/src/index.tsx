2fd5a00463db28e012960c008f1675f3039aa61b
clayton <me@clayton.cc>
2021-03-06 17:12:47 -0800

    Update client dependencies and remove bloat

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -29,11 +29,6 @@ render(
   document.getElementById('loved-app'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
-
 /*
 <Route exact path='/'><Submissions /></Route>
 <Route path='/submit'><Submit /></Route>
