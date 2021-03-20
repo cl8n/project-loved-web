0c09450dce762cc98b9fb10869587c24864c0167
clayton <me@clayton.cc>
2021-03-20 16:11:44 -0700

    More GameMode stuff

--- client/src/osu-helpers.ts
+++ client/src/osu-helpers.ts
@@ -1,7 +1,13 @@
 import { GameMode } from './interfaces';
 
-const shortNames = ['osu', 'taiko', 'fruits', 'mania'] as const;
+export const gameModes = [0, 1, 2, 3] as const;
+export const gameModeShortNames = ['osu', 'taiko', 'fruits', 'mania'] as const;
+export const gameModeLongNames = ['osu!standard', 'osu!taiko', 'osu!catch', 'osu!mania'] as const;
 
 export function gameModeShortName(gameMode: GameMode) {
-  return shortNames[gameMode];
+  return gameModeShortNames[gameMode];
+}
+
+export function gameModeLongName(gameMode: GameMode) {
+  return gameModeLongNames[gameMode];
 }
