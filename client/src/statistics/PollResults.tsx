d098f23835df4270a04718f5aabaf90e568de1e4
clayton <me@clayton.cc>
2021-04-03 16:09:30 -0700

    Add more filtering options to poll results

--- client/src/statistics/PollResults.tsx
+++ client/src/statistics/PollResults.tsx
@@ -7,6 +7,7 @@ import {BeatmapInline} from "../BeatmapInline";
 export default function PollResults() {
   const [gameMode, setGameMode] = useState<GameMode>();
   const [polls, pollsError] = useApi(getPollResults);
+  const [roundOrderAsc, setRoundOrderAsc] = useState(false);
   const [showPercent, setShowPercent] = useState(true);
 
   if (pollsError != null)
@@ -14,10 +15,6 @@ export default function PollResults() {
 
   if (polls == null)
     return <span>Loading poll results...</span>;
-
-  const displayPolls = gameMode == null
-    ? polls
-    : polls.filter((poll) => poll.game_mode === gameMode);
 
   const formatResults = showPercent
     ? (yes: number, no: number) => {
@@ -31,33 +28,49 @@ export default function PollResults() {
   const onGameModeChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const value = event.currentTarget.value;
 
-    setGameMode(value === 'none' ? undefined : parseInt(value));
+    setGameMode(value === 'all' ? undefined : parseInt(value));
   };
+
+  let displayPolls = [...polls];
+
+  if (gameMode != null)
+    displayPolls = displayPolls.filter((poll) => poll.game_mode === gameMode);
+
+  if (roundOrderAsc)
+    displayPolls.sort((a, b) => a.round - b.round);
 
   return (
     <>
       <div className='flex-left'>
-        <label htmlFor='gameMode'>Game mode filter:</label>
+        <label htmlFor='gameMode'>Game mode:</label>
         <select
           name='gameMode'
           value={gameMode}
           onChange={onGameModeChange}
         >
-          <option value='none'>None</option>
+          <option value='all'>All</option>
           {[0, 1, 2, 3].map((m) => (
             <option value={m}>{gameModeLongName(m)}</option>
           ))}
         </select>
-        <button
-          type='button'
-          className='fake-a'
-          onClick={() => setShowPercent((prev) => !prev)}
+        <label htmlFor='roundOrder'>Round order:</label>
+        <select
+          name='roundOrder'
+          value={roundOrderAsc ? '1' : '0'}
+          onChange={(event) => setRoundOrderAsc(event.currentTarget.value === '1')}
         >
-          {showPercent
-            ? 'Display results as "Yes" and "No"'
-            : 'Display results as "Percent" and "Total"'
-          }
-        </button>
+          <option value='0'>Descending</option>
+          <option value='1'>Ascending</option>
+        </select>
+        <label htmlFor='resultDisplay'>Result display:</label>
+        <select
+          name='resultDisplay'
+          value={showPercent ? '0' : '1'}
+          onChange={(event) => setShowPercent(event.currentTarget.value === '0')}
+        >
+          <option value='0'>Percent and total</option>
+          <option value='1'>Yes and no</option>
+        </select>
       </div>
       <table style={{ marginTop: '1em' }}>
         <tr>
