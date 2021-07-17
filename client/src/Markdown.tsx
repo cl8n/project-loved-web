60f8ef8189405719f9a1083fade6f6d0ff2a228b
clayton <me@clayton.cc>
2021-07-17 04:56:18 -0700

    Fix osu wiki links in markdown

--- client/src/Markdown.tsx
+++ client/src/Markdown.tsx
@@ -1,11 +1,23 @@
 import remarkHtml from 'remark-html';
 import remarkParse from 'remark-parse';
 import unified from 'unified';
+import visit from 'unist-util-visit';
 
 const processor = unified()
   .use(remarkParse)
+  .use(osuWikiLinks)
   .use(remarkHtml)
   .freeze();
+
+function osuWikiLinks(): unified.Transformer {
+  return (tree) => {
+    visit(tree, ['image', 'link'], (node: any) => {
+      if (node.url.startsWith('/wiki/')) {
+        node.url = 'https://osu.ppy.sh' + node.url;
+      }
+    });
+  };
+}
 
 interface MarkdownProps {
   text: string;
