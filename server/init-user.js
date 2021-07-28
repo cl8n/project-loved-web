dd7aa7229a1db13eef099ab2c6a13246d7ba4d4e
clayton <me@clayton.cc>
2021-07-28 00:50:11 -0700

    Add script to create an admin user

---
+++ server/init-user.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+if (process.argv.length !== 3) {
+  console.error('Usage: init-user.js <osu! username>');
+  process.exit(1);
+}
+
+require('dotenv').config();
+const db = require('./db');
+const { Osu } = require('./osu');
+
+const osu = new Osu();
+const username = process.argv[2];
+
+(async () => {
+  await Promise.all([
+    db.connect(),
+    osu.getClientCredentialsToken(),
+  ]);
+
+  const user = await osu.createOrRefreshUser(username, true);
+
+  if (user == null) {
+    console.error('Invalid username');
+    process.exit(1);
+  }
+
+  await db.query('DELETE FROM user_roles WHERE id = ?', user.id);
+  await db.query('INSERT INTO user_roles SET ?', { id: user.id, god: true });
+
+  console.log(`Added ${user.name} [#${user.id}] as administrator`);
+
+  await db.close();
+})();
