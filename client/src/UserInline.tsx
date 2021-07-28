4c9a8565f8c827396722a3bf69ba625611c7246a
Liam DeVoe <orionldevoe@gmail.com>
2021-07-28 11:09:42 -0400

    fix flags for new twemoji update

--- client/src/UserInline.tsx
+++ client/src/UserInline.tsx
@@ -6,6 +6,16 @@ type UserInlineProps = {
   user: IUserWithoutRoles;
 };
 
+// https://github.com/ppy/osu-web/blob/7dfcbd2faa4909df6954f033734069fd33d48734/resources/assets/lib/flag-country.tsx#L12-L19
+function flagUrl(countryCode: string) {
+  const fileName = countryCode
+    .split('')
+    .map((c) => (c.charCodeAt(0) + 127397).toString(16))
+    .join('-');
+
+  return `https://osu.ppy.sh/assets/images/flags/${fileName}.svg`;
+}
+
 export function UserInline(props: UserInlineProps) {
   return (
     <a className='no-wrap' href={`https://osu.ppy.sh/users/${props.user.id}`}>
@@ -13,10 +23,12 @@ export function UserInline(props: UserInlineProps) {
         <>
           <img
             alt={`${props.user.country} flag`}
-            src={`https://osu.ppy.sh/images/flags/${props.user.country}.png`}
+            src={flagUrl(props.user.country)}
             style={{
-              height: '1em',
-              paddingBottom: '0.05em',
+              height: '1.4em',
+              // svgs osuweb provides have transparent space on the bottom and top so account for that with negative margin
+              marginBottom: '-0.075em',
+              marginTop: '-0.075em',
               verticalAlign: 'bottom',
             }}
           />
