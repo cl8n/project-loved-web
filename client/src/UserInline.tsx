3b0b3daad5b790c757545a4b009d1b6350acd5c6
clayton <me@clayton.cc>
2021-08-03 10:56:07 -0700

    Allow overridden name in UserInline

--- client/src/UserInline.tsx
+++ client/src/UserInline.tsx
@@ -2,17 +2,18 @@ import CountryFlag from './CountryFlag';
 import { IUserWithoutRoles } from './interfaces';
 
 type UserInlineProps = {
+  name?: string;
   showId?: boolean;
   user: IUserWithoutRoles;
 };
 
-export function UserInline(props: UserInlineProps) {
+export function UserInline({ name, showId, user }: UserInlineProps) {
   return (
-    <a className='no-wrap' href={`https://osu.ppy.sh/users/${props.user.id}`}>
-      <CountryFlag country={props.user.country} />
-      {` ${props.user.name}`}
-      {props.showId &&
-        ` [#${props.user.id}]`
+    <a className='no-wrap' href={`https://osu.ppy.sh/users/${user.id}`}>
+      <CountryFlag country={user.country} />
+      {` ${name ?? user.name}`}
+      {showId &&
+        ` [#${user.id}]`
       }
     </a>
   );
