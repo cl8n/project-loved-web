ab99da8c03ebefd03efc60825ffb3ccdd7920180
clayton <me@clayton.cc>
2021-08-03 10:57:27 -0700

    Add game mode short name => ID helper function

--- client/src/osu-helpers.ts
+++ client/src/osu-helpers.ts
@@ -1,11 +1,17 @@
 import { GameMode } from './interfaces';
 
 export const gameModes = [0, 1, 2, 3] as const;
-export const gameModeShortNames = ['osu', 'taiko', 'fruits', 'mania'] as const;
-export const gameModeLongNames = ['osu!standard', 'osu!taiko', 'osu!catch', 'osu!mania'] as const;
+const gameModeShortNames = ['osu', 'taiko', 'fruits', 'mania'] as const;
+const gameModeLongNames = ['osu!standard', 'osu!taiko', 'osu!catch', 'osu!mania'] as const;
 
 export function gameModeShortName(gameMode: GameMode) {
   return gameModeShortNames[gameMode];
+}
+
+export function gameModeFromShortName(shortName: string) {
+  const gameMode = (gameModeShortNames as any).indexOf(shortName);
+
+  return gameMode < 0 ? null : (gameMode as GameMode);
 }
 
 export function gameModeLongName(gameMode: GameMode) {
