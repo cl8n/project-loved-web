20285ec58938a395fac9243c5fb310dc5547430d
clayton <me@clayton.cc>
2021-08-12 18:08:11 -0700

    Also create sessions table if application didn't create it first

--- server/migrations/023-fix-character-sets-and-collations.sql
+++ server/migrations/023-fix-character-sets-and-collations.sql
@@ -19,5 +19,15 @@ ALTER TABLE `submissions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_
 ALTER TABLE `user_roles` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
 ALTER TABLE `users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
 
+-- In case the sessions table hasn't been created by the application yet
+-- https://github.com/chill117/express-mysql-session/blob/50008e5b71d884b4b7d1b8bec2a306ea296fdf6c/schema.sql
+CREATE TABLE IF NOT EXISTS `sessions` (
+    `session_id` VARCHAR(128) NOT NULL,
+    `expires` INT(11) UNSIGNED NOT NULL,
+    `data` MEDIUMTEXT,
+
+    PRIMARY KEY (`session_id`)
+);
+
 -- Binary collation is sufficient for sessions
 ALTER TABLE `sessions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_bin;
