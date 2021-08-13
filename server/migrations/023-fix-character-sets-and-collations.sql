1b856a161efc6124e0105e93a467916040e5b15a
clayton <me@clayton.cc>
2021-08-12 17:31:31 -0700

    Fix character sets and collations in database

---
+++ server/migrations/023-fix-character-sets-and-collations.sql
@@ -0,0 +1,23 @@
+ALTER DATABASE `project_loved` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+
+ALTER TABLE `beatmaps` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `beatmapset_creators` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `beatmapsets` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `log_values` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `logs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `mapper_consent_beatmapsets` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `mapper_consents` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `nomination_assignees` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `nomination_excluded_beatmaps` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `nomination_nominators` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `nominations` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `poll_results` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `reviews` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `round_game_modes` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `rounds` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `submissions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `user_roles` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+ALTER TABLE `users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
+
+-- Binary collation is sufficient for sessions
+ALTER TABLE `sessions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_bin;
