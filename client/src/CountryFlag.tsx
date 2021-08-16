70cdc18ec8e7f5d4feb861dd1720659fa043b899
clayton <me@clayton.cc>
2021-08-16 14:00:28 -0700

    Cache flags better with webpack

--- client/src/CountryFlag.tsx
+++ client/src/CountryFlag.tsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from 'react';
 import missingFlag from './images/flags/__.png';
 
+let flags: Record<string, string> | undefined;
+const flagsPromise = import('./flags').then(({ default: flagsExport }) => { flags = flagsExport });
+
 type CountryFlagProps = {
   country: string;
 };
 
 export default function CountryFlag({ country }: CountryFlagProps) {
-  const [src, setSrc] = useState(missingFlag);
+  const [src, setSrc] = useState(flags?.[country]);
 
   useEffect(() => {
-    import('./flags').then(({ flagSrc }) => setSrc(flagSrc(country)));
-  }, [country]);
+    if (src == null)
+      flagsPromise.then(() => setSrc(flags?.[country]));
+  }, [country, src]);
 
-  return (
-    <img
-      alt={`${country} flag`}
-      src={src}
-    />
-  );
+  return src == null
+    ? <img alt='Unknown country flag' src={missingFlag} />
+    : <img alt={`${country} flag`} src={src} />
 }
--- client/src/flags.ts
+++ client/src/flags.ts
@@ -1,9 +1,8 @@
-import missingFlag from './images/flags/__.png';
+const flags: Record<string, string> = {};
+const requireFn = (require as any).context('./images/flags', false, /[a-z]{2}\.png$/);
 
-export function flagSrc(country: string) {
-  try {
-    return require(`./images/flags/${country.toLowerCase()}.png`).default;
-  } catch {
-    return missingFlag;
-  }
+for (const flagPath of requireFn.keys() as string[]) {
+  flags[flagPath.slice(-6, -4).toUpperCase()] = requireFn(flagPath).default;
 }
+
+export default flags;
