758d270c7bdb6257800ab74584a073a4ba47808e
clayton <me@clayton.cc>
2021-08-27 18:01:59 -0700

    Allow anonymous user in UserInline

--- client/src/UserInline.tsx
+++ client/src/UserInline.tsx
@@ -2,13 +2,25 @@ import { PropsWithChildren } from 'react';
 import CountryFlag from './CountryFlag';
 import { IUserWithoutRoles } from './interfaces';
 
-type UserInlineProps = {
+const anonymousUser = {
+  id: 4294967296,
+  avatar_url: '',
+  banned: true,
+  country: '__',
+  name: 'Anonymous',
+} as const;
+
+interface UserInlineProps {
   name?: string;
   showId?: boolean;
-  user: IUserWithoutRoles;
-};
+  user: IUserWithoutRoles | null;
+}
 
 export function UserInline({ name, showId, user }: UserInlineProps) {
+  if (user == null) {
+    user = anonymousUser;
+  }
+
   if (user.banned && user.id < 4294000000) {
     showId = true;
   }
@@ -24,7 +36,11 @@ export function UserInline({ name, showId, user }: UserInlineProps) {
   );
 }
 
-function UserInlineContainer({ children, user }: Pick<PropsWithChildren<UserInlineProps>, 'children' | 'user'>) {
+interface UserInlineContainerProps {
+  user: IUserWithoutRoles;
+}
+
+function UserInlineContainer({ children, user }: PropsWithChildren<UserInlineContainerProps>) {
   return user.banned
     ? <span className='no-wrap'>{children}</span>
     : <a className='no-wrap' href={`https://osu.ppy.sh/users/${user.id}`}>{children}</a>;
