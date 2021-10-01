3b874c99d97081fa922a9f6e2ec12d6bc72fa48f
clayton <me@clayton.cc>
2021-09-30 19:07:01 -0700

    Fix news author ID migration

--- server/migrations/028-update-rounds-add-news-author-id.sql
+++ server/migrations/028-update-rounds-add-news-author-id.sql
@@ -1,4 +1,9 @@
 ALTER TABLE `rounds`
-    ADD `news_author_id` INT UNSIGNED NOT NULL AFTER `name`,
+    ADD `news_author_id` INT UNSIGNED NOT NULL AFTER `name`;
+
+-- Random default that will make it obvious it needs to be updated
+UPDATE `rounds` SET `news_author_id` = 3;
+
+ALTER TABLE `rounds`
     ADD FOREIGN KEY (`news_author_id`)
         REFERENCES `users` (`id`);
