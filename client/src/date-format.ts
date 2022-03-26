e625a1f79b170d729a37414ef773e42d30270abe
clayton <me@clayton.cc>
2022-03-26 14:38:56 -0700

    Use capture group instead of lookbehind in regex replace
    Not supported on some browsers apparently.

--- client/src/date-format.ts
+++ client/src/date-format.ts
@@ -6,7 +6,7 @@ function fullIsoString(dateOrIsoString: Date | string) {
   }
 
   return (
-    dateOrIsoString.replace(/(?<=^\d{4}-\d{2}-\d{2}) /, 'T') +
+    dateOrIsoString.replace(/^(\d{4}-\d{2}-\d{2}) /, '$1T') +
     isoStringTemplate.slice(dateOrIsoString.length)
   );
 }
