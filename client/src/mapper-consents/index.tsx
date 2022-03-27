bebc0012e2e964a5c09b6e560ab9461abe042fde
clayton <me@clayton.cc>
2022-03-26 21:36:19 -0700

    Add description to mapper consents page

--- client/src/mapper-consents/index.tsx
+++ client/src/mapper-consents/index.tsx
@@ -146,6 +146,11 @@ export default function MapperConsents() {
         description='Mapper consents page title'
         tagName='h1'
       />
+      <FormattedMessage
+        defaultMessage="Mappers are usually made aware of their maps' usage in the Loved category. This table shows each mapper's consent to let Project Loved put their maps up for voting and potentially add leaderboards to them. Note that mappers may request their maps' removal from Loved at any time, even if their usage was consented to at an earlier time."
+        description='Mapper consents description'
+        tagName='p'
+      />
       {authUser != null && (
         <div className='flex-bar'>
           <MapperConsentEditor
