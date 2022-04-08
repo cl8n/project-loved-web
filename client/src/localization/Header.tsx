e2952540c3ad6247c90a12e94674439b29e4f6ea
clayton <me@clayton.cc>
2022-04-08 15:10:18 -0700

    Update localization header instructions

--- client/src/localization/Header.tsx
+++ client/src/localization/Header.tsx
@@ -10,21 +10,23 @@ export default function Header() {
       />
       <p>To translate this website:</p>
       <ul>
-        <li>Select your locale below, or manually input the BCP 47 tag if it doesn't exist yet</li>
+        <li>Select your locale below, or manually input the BCP 47 tag if it doesn't exist yet.</li>
         <li>
-          Translate the messages (you don't have to do all at once). Keep any colored text verbatim
-          from the English messages. If you need to use special language for plural types, gendered
-          words, etc., look up how to use ICU MessageFormat to do this.
+          Translate the messages. Keep any colored text verbatim from the English messages. If you
+          need to use variations of language for plural types, gendered words, etc., ask a developer
+          if there are supported options for that message.
         </li>
-        <li>Click "Export messages"</li>
+        <li>Click "Export messages". A download containing your translations will start.</li>
         <li>
-          Either commit the resulting file to a fork of{' '}
-          <a href='https://github.com/cl8n/project-loved-web'>project-loved-web</a> and open a pull
-          request, or send it to someone else who can do that for you
+          If you're comfortable using git and GitHub, commit the file to a fork of{' '}
+          <a href='https://github.com/cl8n/project-loved-web'>project-loved-web</a> at{' '}
+          <code>client/src/translations/[locale].json</code>, and open a pull request. If not, send
+          the file to a developer or one of the public channels.
         </li>
         <li>
-          Keep in mind that (for now) this page doesn't save any of your work. Anything you type
-          into the boxes will be cleared when you close the page or navigate away.
+          Keep in mind that <b>this page doesn't save any of your work</b>. Anything you type into
+          the boxes will be cleared when you close the page, navigate away, or switch to a different
+          locale.
         </li>
       </ul>
     </>
