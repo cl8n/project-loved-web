617e8bdb5e32acf6e3ead7d812b09afb19cae666
Irisu <nekomarisakirisame@gmail.com>
2022-09-28 02:19:06 -0400

    update tooltip to have a container

--- client/src/Tooltip.tsx
+++ client/src/Tooltip.tsx
@@ -1,28 +1,43 @@
 import type { PropsWithChildren, ReactNode } from 'react';
-import { useState } from 'react';
+import { createRef, useLayoutEffect, useState } from 'react';
 
 interface TooltipProps {
   content: ReactNode;
 }
 
 export default function Tooltip({ children, content }: PropsWithChildren<TooltipProps>) {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
+  const tooltipRef = createRef<HTMLDivElement>();
+  const tooltipContainerRef = createRef<HTMLDivElement>();
+
+  useLayoutEffect(() => {
+    // position the tooltip to the tooltip container
+    const tooltip = tooltipRef.current;
+    const tooltipContainer = tooltipContainerRef.current;
+
+    if (tooltip && tooltipContainer) {
+      tooltip.style.transform = `translateX(-${tooltipContainer.offsetWidth / 2}px)`;
+    }
+  }, [visible]);
 
   return (
     <div
-      className='tooltip'
+      className='tooltip-container'
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
       onTouchEnd={() => setVisible((prev) => !prev)}
+      ref={tooltipContainerRef}
     >
       {children}
-      {visible && (
-        <div className='tooltip-content'>
-          {content}
-          <div className='tooltip-hover-area' />
-          <div className='tooltip-triangle' />
-        </div>
-      )}
+      <div className='tooltip' ref={tooltipRef}>
+        {visible && (
+          <div className='tooltip-content'>
+            {content}
+            <div className='tooltip-hover-area' />
+            <div className='tooltip-triangle' />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
