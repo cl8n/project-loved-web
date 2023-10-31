68cf92f8b395ea9076bc05e2545db95be4c35571
clayton <me@clayton.cc>
2023-10-31 02:33:31 -0700

    Improve tooltip layout
    Closes #79

--- client/src/Tooltip.tsx
+++ client/src/Tooltip.tsx
@@ -1,26 +1,45 @@
 import type { PropsWithChildren, ReactNode } from 'react';
 import { createRef, useLayoutEffect, useState } from 'react';
 
+const halfEmInPx = 7;
+
 interface TooltipProps {
   content: ReactNode;
 }
 
 export default function Tooltip({ children, content }: PropsWithChildren<TooltipProps>) {
+  const [position, setPosition] = useState('above');
   const [visible, setVisible] = useState(false);
-  const tooltipOuterRef = createRef<HTMLDivElement>();
   const tooltipRef = createRef<HTMLDivElement>();
+  const tooltipTriangleRef = createRef<HTMLDivElement>();
 
   useLayoutEffect(() => {
     const tooltip = tooltipRef.current;
-    const tooltipOuter = tooltipOuterRef.current;
+    const tooltipTriangle = tooltipTriangleRef.current;
 
-    if (tooltip && tooltipOuter) {
-      const rect = tooltipOuter.getBoundingClientRect();
+    if (tooltip != null && tooltipTriangle != null) {
+      let margin: number | undefined;
+      const tooltipClientRect = tooltip.getBoundingClientRect();
+      const leftAdjust = halfEmInPx - tooltipClientRect.left;
+      const rightAdjust =
+        tooltipClientRect.right - (document.documentElement.clientWidth - halfEmInPx);
 
-      tooltip.style.bottom = `${rect.height}px`;
-      tooltip.style.left = `${rect.width / 2}px`;
+      if (leftAdjust > 0) {
+        margin = leftAdjust;
+      } else if (rightAdjust > 0) {
+        margin = -rightAdjust;
+      }
+
+      if (margin != null) {
+        tooltip.style.marginLeft = `${margin}px`;
+        tooltipTriangle.style.marginLeft = `${-margin}px`;
+      }
+
+      if (tooltipClientRect.top < halfEmInPx) {
+        setPosition('below');
+      }
     }
-  }, [tooltipOuterRef, tooltipRef, visible]);
+  }, [tooltipRef, tooltipTriangleRef, visible]);
 
   return (
     <div
@@ -28,16 +47,13 @@ export default function Tooltip({ children, content }: PropsWithChildren<Tooltip
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
       onTouchEnd={() => setVisible((prev) => !prev)}
-      ref={tooltipOuterRef}
     >
       {children}
       {visible && (
-        <div className='tooltip-container'>
-          <div className='tooltip' ref={tooltipRef}>
-            {content}
-            <div className='tooltip__hover-area' />
-            <div className='tooltip__triangle' />
-          </div>
+        <div className={`tooltip tooltip--${position ?? 'above'}`} ref={tooltipRef}>
+          {content}
+          <div className='tooltip__hover-area' />
+          <div className='tooltip__triangle' ref={tooltipTriangleRef} />
         </div>
       )}
     </div>
