b1bd07dca3859cb1c84a69fe6b8f0309021ef7e4
clayton <me@clayton.cc>
2024-06-22 01:10:39 -0700

    More correct deps invalidation

--- server/src/cache.ts
+++ server/src/cache.ts
@@ -1,21 +1,20 @@
-const cacheStore = new Map<string, { cachedAt: number; value: unknown }>();
-const usedBy = new Map<string, string[]>();
+const cacheStore: Partial<Record<string, { cachedAt: number; value: unknown }>> = {};
+const usedBy: Partial<Record<string, string[]>> = {};
+const usedByFilled = new Set<string>();
 
 export function cache<T>(
   { dependsOn, key, ttlSeconds }: { dependsOn?: string[]; key: string; ttlSeconds: number },
   fn: () => T,
 ): T {
-  // If this key isn't in the dependency map, then create it, and set up all of
-  // its reverse dependencies.
-  if (!usedBy.has(key)) {
-    usedBy.set(key, []);
-
+  if (!usedByFilled.has(key)) {
     for (const dependency of dependsOn ?? []) {
-      usedBy.get(dependency)?.push(key);
+      (usedBy[dependency] ??= []).push(key);
     }
+
+    usedByFilled.add(key);
   }
 
-  const cacheItem = cacheStore.get(key);
+  const cacheItem = cacheStore[key];
   const now = Date.now() / 1000;
 
   if (cacheItem != null) {
@@ -27,14 +26,11 @@ export function cache<T>(
   }
 
   const value = fn();
-  cacheStore.set(key, { cachedAt: now, value });
+  cacheStore[key] = { cachedAt: now, value };
   return value;
 }
 
 export function deleteCache(key: string): void {
-  cacheStore.delete(key);
-
-  for (const reverseDependency of usedBy.get(key) ?? []) {
-    deleteCache(reverseDependency);
-  }
+  delete cacheStore[key];
+  usedBy[key]?.forEach(deleteCache);
 }
