ba4f690f7a3238469da34e5fde57a0e1e1d454dc
clayton <me@clayton.cc>
2024-09-06 02:25:40 -0700

    Simplify log format when actor is the same as user

--- client/src/manage/logs/LogMessage.tsx
+++ client/src/manage/logs/LogMessage.tsx
@@ -18,14 +18,26 @@ const logTemplates = {
   [LogType.roleCreated]: '{actor} created role {role} on {user}',
   [LogType.roleDeleted]: '{actor} deleted role {role} from {user}',
   [LogType.roleToggledAlumni]: '{actor} {markedOrUnmarked} role {role} as alumni on {user}',
-  [LogType.mapperConsentCreated]: '{actor} created mapper consent for {user}',
-  [LogType.mapperConsentUpdated]: '{actor} updated mapper consent for {user}',
-  [LogType.mapperConsentBeatmapsetCreated]:
-    '{actor} created mapper consent for {user} on {beatmapset}',
-  [LogType.mapperConsentBeatmapsetDeleted]:
-    '{actor} deleted mapper consent for {user} on {beatmapset}',
-  [LogType.mapperConsentBeatmapsetUpdated]:
-    '{actor} updated mapper consent for {user} on {beatmapset}',
+  [LogType.mapperConsentCreated]: {
+    self_false: '{actor} created mapper consent for {user}',
+    self_true: '{user} created mapper consent',
+  },
+  [LogType.mapperConsentUpdated]: {
+    self_false: '{actor} updated mapper consent for {user}',
+    self_true: '{user} updated mapper consent',
+  },
+  [LogType.mapperConsentBeatmapsetCreated]: {
+    self_false: '{actor} created mapper consent for {user} on {beatmapset}',
+    self_true: '{user} created mapper consent on {beatmapset}',
+  },
+  [LogType.mapperConsentBeatmapsetDeleted]: {
+    self_false: '{actor} deleted mapper consent for {user} on {beatmapset}',
+    self_true: '{user} deleted mapper consent on {beatmapset}',
+  },
+  [LogType.mapperConsentBeatmapsetUpdated]: {
+    self_false: '{actor} updated mapper consent for {user} on {beatmapset}',
+    self_true: '{user} updated mapper consent on {beatmapset}',
+  },
   [LogType.settingUpdated]: '{actor} updated setting {setting}',
   [LogType.extraTokenCreated]: '{user} created extra token with scopes {scopes}',
   [LogType.extraTokenDeleted]: {
@@ -39,12 +51,15 @@ const logTemplates = {
   [LogType.pollUpdated]:
     '{actor} updated results for {gameMode} poll {poll} on {beatmapset} for the round of {round}',
   [LogType.submissionDeleted]: {
-    user_false: '{actor} deleted {gameMode} submission on {beatmapset}',
-    user_true: '{actor} deleted {gameMode} submission for {user} on {beatmapset}',
+    user_false: '{actor} deleted {gameMode} submission on {beatmapset} (no user attached)',
+    user_true_self_false: '{actor} deleted {gameMode} submission for {user} on {beatmapset}',
+    user_true_self_true: '{user} deleted {gameMode} submission on {beatmapset}',
   },
   [LogType.reviewCreated]: '{user} created {gameMode} review with {score} on {beatmapset}',
-  [LogType.reviewDeleted]:
-    '{actor} deleted {gameMode} review with {score} for {user} on {beatmapset}',
+  [LogType.reviewDeleted]: {
+    self_false: '{actor} deleted {gameMode} review with {score} for {user} on {beatmapset}',
+    self_true: '{user} deleted {gameMode} review with {score} on {beatmapset}',
+  },
   [LogType.reviewUpdated]: '{user} updated {gameMode} review with {from} to {to} on {beatmapset}',
   [LogType.beatmapsetCreated]: '{actor} created beatmapset {beatmapset}',
   [LogType.beatmapsetDeleted]: '{actor} deleted beatmapset {beatmapset}',
@@ -113,21 +128,43 @@ export default function LogMessage(log: Log) {
   let match: RegExpExecArray | null;
   let lastMatchEnd = 0;
 
-  if (log.type === LogType.extraTokenDeleted) {
-    if (log.values != null) {
-      const hasActor = log.values.actor != null;
-      const hasScopes = Array.isArray(log.values.scopes) && log.values.scopes.length > 0;
+  switch (log.type) {
+    case LogType.mapperConsentCreated:
+    case LogType.mapperConsentUpdated:
+    case LogType.mapperConsentBeatmapsetCreated:
+    case LogType.mapperConsentBeatmapsetDeleted:
+    case LogType.mapperConsentBeatmapsetUpdated:
+    case LogType.reviewDeleted:
+      if (log.values != null) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const actorIsUser = (log.values.actor as any).id === (log.values.user as any).id;
 
-      template = logTemplates[log.type][`actor_${hasActor}_scopes_${hasScopes}`];
-    }
-  } else if (log.type === LogType.submissionDeleted) {
-    if (log.values != null) {
-      const hasUser = log.values.user != null;
+        template = logTemplates[log.type][`self_${actorIsUser}`];
+      }
+      break;
+    case LogType.extraTokenDeleted:
+      if (log.values != null) {
+        const hasActor = log.values.actor != null;
+        const hasScopes = Array.isArray(log.values.scopes) && log.values.scopes.length > 0;
 
-      template = logTemplates[log.type][`user_${hasUser}`];
-    }
-  } else {
-    template = logTemplates[log.type] ?? '{invalid}';
+        template = logTemplates[log.type][`actor_${hasActor}_scopes_${hasScopes}`];
+      }
+      break;
+    case LogType.submissionDeleted:
+      if (log.values != null) {
+        if (log.values.user == null) {
+          template = logTemplates[log.type].user_false;
+        } else {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const actorIsUser = (log.values.actor as any).id === (log.values.user as any).id;
+
+          template = logTemplates[log.type][`user_true_self_${actorIsUser}`];
+        }
+      }
+      break;
+    default:
+      template = logTemplates[log.type] ?? '{invalid}';
+      break;
   }
 
   if (log.values != null) {
